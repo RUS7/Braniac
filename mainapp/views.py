09ac0ede835c9a5ad1c98c5e3e641dff52c65614
Rustam <kadastr056@yandex.ru>
2022-08-30 23:03:07 +0300

    Home work lesson three

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.views import View
 from django.views.generic import TemplateView
@@ -9,6 +11,21 @@ class MainPageView(TemplateView):
 
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["news_title"] = "Загрузим в будущем БД"
+        context["news_preview"] = "Описание новости"
+        context["range"] = range(5)
+        context["data_obj"] = datetime.now()
+        return context
+
+
+class NewsWithPaginatorView(NewsPageView):
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
+        return context
 
 
 class CoursesListPageView(TemplateView):
