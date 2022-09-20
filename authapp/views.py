bf14f3d1d28f27b74bd8e83b9c613cea7843e270
Rustam <kadastr056@yandex.ru>
2022-09-20 18:06:49 +0300

    HW

--- authapp/views.py
+++ authapp/views.py
@@ -1,14 +1,20 @@
+
+
+from axes.decorators import axes_dispatch
 from django.contrib import messages
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, UpdateView
 
 from authapp import forms
 
+@method_decorator(axes_dispatch, name='dispatch')
 
 class CustomLoginView(LoginView):
     def form_valid(self, form):
@@ -52,3 +58,8 @@ class ProfileEditView(UserPassesTestMixin, UpdateView):
 
     def get_success_url(self):
         return reverse_lazy("authapp:profile_edit", args=[self.request.user.pk])
+
+class PasswordsChangeView(PasswordChangeView):
+    template_name = 'authapp/change_password.html'
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('mainapp:main_page')
